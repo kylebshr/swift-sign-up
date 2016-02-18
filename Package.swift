93329989c5e2023e1c036972c435dccb23b8e36d
Kyle Bashour <kylebshr@me.com>
2016-02-17 21:45:18 -0500

    Switch pgsql packages

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 0),
         .Package(url: "https://github.com/tannernelson/vapor-stencil.git", majorVersion: 0),
-        .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0)
+        .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0, minor: 2)
     ]
+
 )
