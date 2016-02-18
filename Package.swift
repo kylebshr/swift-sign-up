e49ef8a461ca9d15258bbc4151cf8c63a817ea8f
Kyle Bashour <kylebshr@me.com>
2016-02-17 22:05:26 -0500

    Move to custom pgsql repo

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 0),
         .Package(url: "https://github.com/tannernelson/vapor-stencil.git", majorVersion: 0),
-        .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0)
+        .Package(url: "https://github.com/kylebshr/PostgreSQL-Swift.git", majorVersion: 0)
     ]
 
 )
