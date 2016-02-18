7c81541929f753ebae27683090bc89eb302a2835
Kyle Bashour <kylebshr@me.com>
2016-02-17 21:12:04 -0500

    Add PG package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/tannernelson/vapor-stencil.git", majorVersion: 0)
+        .Package(url: "https://github.com/tannernelson/vapor-stencil.git", majorVersion: 0),
+        .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0)
     ]
 )
